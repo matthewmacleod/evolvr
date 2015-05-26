0082bd04c4fe7ee51183563dbb435e6ec62b2ebe
Matthew K. MacLeod <matthewmacleod28@gmail.com>
2015-05-25 18:20:37 -0600

    I like the test better with import

--- test/bio_test.exs
+++ test/bio_test.exs
@@ -1,9 +1,18 @@
+
+import Bio
+
 defmodule BioTest do
   use ExUnit.Case
 
-  test "should return count of a particular pattern in a sequence..including overlaps" do
-    pc = Bio.pattern_count("ACAACTATGCATACTATCGGGAACTATCCT","ACTAT")
+  test "should return count of a particular pattern in a sequence where count includes overlaps" do
+    pc = pattern_count("ACAACTATGCATACTATCGGGAACTATCCT","ACTAT")
     assert pc == 3
+  end
+
+  test "should break up sequence into an array of k chunks" do
+    seq = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    ans = ["ABC", "DEF", "GHI", "JKL", "MNO", "PQR", "STU", "VWX", "YZ"]
+    assert get_kmers(seq,3) == ans
   end
 
 
